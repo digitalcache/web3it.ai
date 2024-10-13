4f09e2357f207a4ccc862b79d3f13cc98895ec11
charlis-fl <charlis@founderandlightning.com>
2024-10-13 13:02:55 +0530

    hello home

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -5,6 +5,7 @@ import { Footer } from '@/common/components/organisms';
 
 
 export default function Home () {
+  console.log("hello")
   return (
     <div className="min-h-screen overflow-hidden">
       <Hero />
