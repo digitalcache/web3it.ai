3dd3786f4ab1ca29e4ec24c0887fab7a7d52a472
charlis-fl <charlis@founderandlightning.com>
2024-11-01 10:21:46 +0530

    submdomain cost bug fix

--- frontend/app/[subdomain]/page.tsx
+++ frontend/app/[subdomain]/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/common/utils/supabase/client';
 import { readContract } from '@wagmi/core'
 import { Metadata } from 'next';
 import { config } from '@/config';
-// import { ethers } from "ethers";
+import { ethers } from "ethers";
 import { Address } from 'viem';
 import { ContractFunctions } from '@/common/constants';
 import { IdeaType } from '@/common/types';
@@ -46,13 +46,12 @@ const TokenDetail = async ({ params } : {
   const supabase = createClient();
   const { data: subdomains } = await supabase.from('Subdomains').select('*')
 
-  async function costBasedOnTokens () {
+  async function costBasedOnTokens (totalSupply: number, purchaseAmount: number) {
     'use server';
-    return 300
-    // const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
-    // const contract = new ethers.Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || '', ideaAbi, provider);
-    // const costInWei = await contract.calculateCost(totalSupply, purchaseAmount);
-    // return costInWei
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+    const contract = new ethers.Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || '', ideaAbi, provider);
+    const costInWei = await contract.calculateCost(totalSupply, purchaseAmount);
+    return costInWei
   }
 
   if (subdomains?.length) {
